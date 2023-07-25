75b9fca6d80be685c0f1b6eeea6f43030b0da235
peerhoffmanncode <peerhoffmann81@gmail.com>
2023-07-25 13:27:10 -0700

    Kata Cockroach - Kata FindInArray

---
+++ CockRoach/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Cockroach.CockroachSpeed(1.08) == 30);
+        Console.WriteLine(Cockroach.CockroachSpeed(1.09) == 30);
+        Console.WriteLine(Cockroach.CockroachSpeed(0) == 0);
+
+        Console.ReadKey();
+    }
+}
+
+public class Cockroach
+{
+    public static int CockroachSpeed(double x) => (int)(x * 100000) / (60 * 60);
+}
---
+++ FindItemInArray/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(true == Kata.Check(new object[] { 66, 101 }, 66));
+        Console.WriteLine(true == Kata.Check(new object[] { 80, 117, 115, 104, 45, 85, 112, 115 }, 45));
+        Console.WriteLine(true == Kata.Check(new object[] { 't', 'e', 's', 't' }, 'e'));
+        Console.WriteLine(false == Kata.Check(new object[] { "what", "a", "great", "kata" }, "kat"));
+
+        Console.ReadKey();
+    }
+}
+
+public class Kata
+{
+    public static bool Check(object[] a, object x)
+    {
+        return Array.IndexOf(a, x) != -1;
+    }
+}
